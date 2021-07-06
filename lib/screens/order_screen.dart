42adbb338fe6954c7aeec6a97530ac91059daadd
shikharsaurav <shikharsaurav51@gmail.com>
2021-07-06 19:16:22 +0530

    1st Commit

---
+++ lib/screens/order_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
+  //for stateful widget
+  // var _isLoading = false;
+  // @override
+  // void initState() {
+  // Future.delayed(Duration.zero).then((_) async {
+  //   setState(() {
+  //     _isLoading = true;
+  //   });
+  //   await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  //   setState(() {
+  //     _isLoading = false;
+  //   });
+  // });
+
+  //both above and below work as same
+
+  // _isLoading = true;
+  // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_){
+  //   setState(() {
+  //     _isLoading = false;
+  //   });
+  //});
+
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    print('building orders');
+    //final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapShot.error != null) {
+              return Center(
+                child: Text('An error occured'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, index) => OrderItem(
+                    orderData.orders[index],
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../providers/orders.dart' as ord;
+
+class OrderItem extends StatefulWidget {
+  final ord.OrderItem order;
+
+  OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(
+        milliseconds: 300,
+      ),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: _expanded
+                    ? Icon(Icons.expand_less)
+                    : Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
+            ),
+            AnimatedContainer(
+              duration: Duration(
+                milliseconds: 300,
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 15,
+              ),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 10, 100)
+                  : 0,
+              child: ListView.builder(
+                itemCount: widget.order.products.length,
+                itemBuilder: (ctx, index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '${widget.order.products[index].title}',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${widget.order.products[index].quantity}x \$${widget.order.products[index].price}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
